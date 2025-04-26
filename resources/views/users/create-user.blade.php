1ef57eb87005e6a17953e1af63edcab52412b885
ANTOCAZADOR <antonio.brian.gaytan15@gmail.com>
2025-04-26 00:30:53 -0600

    Implementación plantilla

---
+++ resources/views/users/create-user.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hola</h1>
+</x-layout>
---
+++ resources/views/users/index-user.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="text-center text-white">
+            <h1>¡Bienvenido a Diamante Rojo!</h1>
+            <p class="fs-4 mt-3">La suerte está de tu lado. ¿Estás listo para ganar en grande?</p>
+        </div>
+    </div>
+</x-layout>
